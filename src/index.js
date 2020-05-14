9842a44d1501d4c0c4c60daf0469b3b85eb09471
Tolga Sofuoglu <tolgasofuoglu@gmail.com>
2020-05-14 14:15:36 +0100

    Add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Started');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Api started on port ${port}`));
