f73f83d64fa596ce5face8af17ae7853720895e8
Tolga Sofuoglu <tolgasofuoglu@gmail.com>
2020-05-14 16:50:44 +0100

    Add eslint prettier

--- src/schedule/scheduleTest.js
+++ src/schedule/scheduleTest.js
@@ -1,8 +1,11 @@
 import supertest from "supertest";
 import app from "../app";
 
+const request = supertest(app);
+
 describe("GET /schedules", () => {
-  it("responds with 200", (done) => {
-    supertest(app).get("/schedules").expect(200, done);
+  it("responds with 200", async () => {
+    const response = await request.get("/schedules");
+    expect(response.status).toBe(200);
   });
 });
